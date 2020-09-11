ef5fecf7efb86dc743669e4bb51650f2ba905b28
ErickR3005 <59286962+ErickR3005@users.noreply.github.com>
2020-09-11 12:25:32 -0500

    Main Activity

--- app/src/main/java/com/example/theheroproyect/MainActivity.java
+++ app/src/main/java/com/example/theheroproyect/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.theheroproyect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buscarHeroe() {
+    EditText nombre=findViewById(R.id.et_heroe);
+    if (nombre.getText().toString().length()>=3){
+        String url=" https://www.superheroapi.com/api.php/3612163438818253/search/"+nombre.getText().toString();
+        System.out.println(url);
+    }
+    else{
+        Toast.makeText(this,"Se requiere una longitud minima de 3 letras para buscar",Toast.LENGTH_SHORT).show();
+        nombre.setText("");
+    }
+    }
+    public void busqueda(View view){
+        buscarHeroe();
     }
 }
